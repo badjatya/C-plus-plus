9840bb230698e0ad37c590109d48175db5a0f948
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-02-07 14:28:04 +0530

    addition Program
    A c++ program to add two number

---
+++ basic/01 Addition/add.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a, b, sum;
+
+    cout << "Enter Two Number ";
+    cin >> a >> b;
+
+    sum = a + b;
+    cout << "Sum of " << a << " and " << b << " is " << sum;
+
+    return 0;
+}
