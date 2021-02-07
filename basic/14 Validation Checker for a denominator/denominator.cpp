68c355e7ad08afc17ff363e3e2d2d510307d650a
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-02-08 02:22:45 +0530

    Denominatoe validator

---
+++ basic/14 Validation Checker for a denominator/denominator.cpp
@@ -0,0 +1,25 @@
+/* Take two user inputs and check id denominator is valid or not if valid than find the dividend  */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    cout << "Enter the First Number: ";
+    cin >> a;
+
+    cout << "Enter the Second Number: ";
+    cin >> b;
+
+    if (b == 0)
+        cout << "Invalid Denominator " << endl;
+    else
+    {
+        c = a / b;
+        cout << "Valid Denominator " << endl;
+        cout << "Output is: " << c << endl;
+    }
+    return 0;
+}
