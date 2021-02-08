a4ed8773544f108de9e85c1d7ca4044453bfaf89
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-02-08 13:28:18 +0530

    Calculate Month name based on day Number

---
+++ basic/22 Display Month Name/month.cpp
@@ -0,0 +1,58 @@
+/* Calculate Month name based on day Number  */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int month;
+    cout << "Enter Day Number: ";
+    cin >> month;
+
+    switch (month)
+    {
+    case 1:
+        cout << "January" << endl;
+        break;
+    case 2:
+        cout << "February" << endl;
+        break;
+    case 3:
+        cout << "March" << endl;
+        break;
+    case 4:
+        cout << "April" << endl;
+        break;
+    case 5:
+        cout << "May" << endl;
+        break;
+    case 6:
+        cout << "June" << endl;
+        break;
+    case 7:
+        cout << "July" << endl;
+        break;
+    case 8:
+        cout << "August" << endl;
+        break;
+    case 9:
+        cout << "September" << endl;
+        break;
+    case 10:
+        cout << "October" << endl;
+        break;
+    case 11:
+        cout << "November" << endl;
+        break;
+    case 12:
+        cout << "December" << endl;
+        break;
+
+    default:
+        cout << "Enter Valid Number" << endl;
+        break;
+    }
+
+    return 0;
+}
