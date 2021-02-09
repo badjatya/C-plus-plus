56ea9c4b587ecedffb1515884e4e6e8c17c53b3f
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-02-09 19:02:11 +0530

    Pattern 2

---
+++ basic/42 Patttern/pattern.cpp
@@ -0,0 +1,28 @@
+/* TODO: Printing a pattern using nested for loops  */
+
+/*
+Pattern to be Printed: 
+* 
+* * 
+* * * 
+* * * *
+* * * * *
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
